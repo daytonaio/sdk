62fed4bc7bcda9893f577b494d7c5b7328306376
MDzaja <mirkodzaja0@gmail.com>
2025-03-06 11:49:18 +0000

    refactor: migrate examples from workspace to sandbox
    Signed-off-by: MDzaja <mirkodzaja0@gmail.com>

--- examples/python/exec-command/exec.py
+++ examples/python/exec-command/exec.py
@@ -1,24 +1,25 @@
-from daytona_sdk import Daytona, CreateWorkspaceParams
+from daytona_sdk import Daytona, CreateSandboxParams
 
 daytona = Daytona()
 
-params = CreateWorkspaceParams(
+params = CreateSandboxParams(
     language="python",
 )
-workspace = daytona.create(params)
+sandbox = daytona.create(params)
 
-# Run the code securely inside the workspace
-response = workspace.process.code_run('print("Hello World!")')
+# Run the code securely inside the sandbox
+response = sandbox.process.code_run('print("Hello World!")')
 if response.exit_code != 0:
     print(f"Error: {response.exit_code} {response.result}")
 else:
     print(response.result)
 
-# Execute an os command in the workspace
-response = workspace.process.exec('echo "Hello World from exec!"', cwd="/home/daytona", timeout=10)
+# Execute an os command in the sandbox
+response = sandbox.process.exec(
+    'echo "Hello World from exec!"', cwd="/home/daytona", timeout=10)
 if response.exit_code != 0:
     print(f"Error: {response.exit_code} {response.result}")
 else:
     print(response.result)
 
-daytona.remove(workspace)
+daytona.remove(sandbox)
