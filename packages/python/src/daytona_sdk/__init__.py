af1a7587f0e1ed77dcd4e23284e49982e264b8c1
Mirko Džaja <mirkodzaja0@gmail.com>
2025-04-13 09:43:00 -0700

    feat: upload multiple files (#61)
    Signed-off-by: MDzaja <mirkodzaja0@gmail.com>

--- packages/python/src/daytona_sdk/__init__.py
+++ packages/python/src/daytona_sdk/__init__.py
@@ -2,7 +2,16 @@ from daytona_api_client import WorkspaceState as SandboxState
 
 # Create deprecated aliases with proper warnings
 from ._utils.deprecation import deprecated_alias
-from .charts import BarChart, BoxAndWhiskerChart, Chart, ChartType, CompositeChart, LineChart, PieChart, ScatterChart
+from .charts import (
+    BarChart,
+    BoxAndWhiskerChart,
+    Chart,
+    ChartType,
+    CompositeChart,
+    LineChart,
+    PieChart,
+    ScatterChart,
+)
 from .common.code_run_params import CodeRunParams
 from .daytona import (
     CodeLanguage,
@@ -14,13 +23,20 @@ from .daytona import (
     SessionExecuteRequest,
     SessionExecuteResponse,
 )
+from .filesystem import FileUpload
 from .lsp_server import LspLanguageId
 from .sandbox import Sandbox, SandboxState, SandboxTargetRegion
 
-CreateWorkspaceParams = deprecated_alias("CreateWorkspaceParams", "CreateSandboxParams")(CreateSandboxParams)
+CreateWorkspaceParams = deprecated_alias(
+    "CreateWorkspaceParams", "CreateSandboxParams"
+)(CreateSandboxParams)
 Workspace = deprecated_alias("Workspace", "Sandbox")(Sandbox)
-WorkspaceTargetRegion = deprecated_alias("WorkspaceTargetRegion", "SandboxTargetRegion")(SandboxTargetRegion)
-WorkspaceResources = deprecated_alias("WorkspaceResources", "SandboxResources")(SandboxResources)
+WorkspaceTargetRegion = deprecated_alias(
+    "WorkspaceTargetRegion", "SandboxTargetRegion"
+)(SandboxTargetRegion)
+WorkspaceResources = deprecated_alias("WorkspaceResources", "SandboxResources")(
+    SandboxResources
+)
 WorkspaceState = deprecated_alias("WorkspaceState", "SandboxState")(SandboxState)
 
 __all__ = [
@@ -51,4 +67,5 @@ __all__ = [
     "PieChart",
     "BoxAndWhiskerChart",
     "CompositeChart",
+    "FileUpload",
 ]
